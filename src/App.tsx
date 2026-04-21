6aa5496c70a6807ba9756ecb1343e5c62ecb1e90
Dominic-Bravo <dominicianbravo017@gmail.com>
2026-04-21 11:25:01 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div>
+      <p className="text-amber-900 mt-4 indent-15 text-2xl mt-15 transition-transform duration-500 ease-out hover:scale-105">
+        At our coffee shop, every cup tells a story. From carefully sourced beans to expertly crafted drinks, we turn simple moments into something special. Whether you're here to work, relax, or catch up with friends—there’s always a perfect brew waiting for you.
+      </p>
+    </div>
+  )
+}
