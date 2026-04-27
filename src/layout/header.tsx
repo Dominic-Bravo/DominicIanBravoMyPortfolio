0f3a3a0272c3cdb7b2d93d01e3fcd7c09ada41d3
Dominic-Bravo <dominicianbravo017@gmail.com>
2026-04-27 18:01:58 +0800

    Revert "fix: Changes"
    This reverts commit 98f647d9d13047aae53b1ae6c3e4004f3aff897b.

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -93,7 +93,7 @@ export const Navbar = ({ currentPage, onNavigate }: NavbarProps) => {
                 }}
                 className="block w-full text-left px-3 py-2 rounded-lg bg-emerald-600 text-white font-semibold hover:opacity-90 transition-opacity"
               >
-                My Resume
+                Get in Touch
               </button>
             </div>
           </div>
